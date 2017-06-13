Backpropagation is now correct, also added a more general function to compute derivatives of activation functions.

NeuralNet.py
@@ -6,14 +6,15 @@ import numpy as np
 class NeuralNet:
     
     def __init__(self, 
-                 nr_of_nodes,
                  activation_function,
                  learning_rate,
-                 targets):
-        self.nodes = nr_of_nodes
+                 targets,
+                 batch_size):
         self.acf = act_f(activation_function)
+        self.acf_deriv = act_deriv(activation_function)
         self.lr = learning_rate
         self.targets = targets
+        self.batch_size = batch_size
         
     # Initialize starting weights
     def init_weights(self,
@@ -25,29 +26,41 @@ class NeuralNet:
         self.bias_ih = np.random.rand(nodes_hidden)
         self.bias_ho = np.random.rand(nodes_output)
             
-    # Feed forward step
+    # Feed forward step (with backpropagation)
     def feed_forward(self,
                      data):
         
-#        print ("Initial weights are: ", self.weights_ih, "input -> hidden", "\n")
-#        print (self.weights_ho, "hidden -> output", "\n")
+        idx = np.random.randint(data.shape[0], size=self.batch_size)
+        batch_data = data[idx,:]
+        targets = self.targets[idx]    
+
+        for batch_index in range(batch_size): 
+            weighted_input_hidden_layer = np.dot(self.weights_ih.transpose(), batch_data[batch_index,:]) + self.bias_ih
+            first_layer = self.acf(weighted_input_hidden_layer)
         
-        weighted_input_hidden_layer = np.dot(np.transpose(self.weights_ih), data) + self.bias_ih
-        first_layer = self.acf(weighted_input_hidden_layer)
+            weighted_input_output_layer = np.dot(self.weights_ho.transpose(), first_layer) + self.bias_ho
+            output = self.acf(weighted_input_output_layer)
+            
+            delta_output = np.multiply(compute_errors_prime(output, targets[batch_index]), self.acf_deriv(weighted_input_output_layer))
+            delta_hidden = np.multiply(np.dot(self.weights_ho, delta_output), self.acf_deriv(weighted_input_hidden_layer))
+            
+            self.weights_ho -= self.lr*np.multiply(first_layer, delta_output).reshape((-1,1))
+            self.weights_ih -= self.lr*np.multiply(batch_data[batch_index,:].reshape((-1,1)), delta_hidden)
+            
+            self.bias_ho -= self.lr*delta_output
+            self.bias_ih -= self.lr*delta_hidden
+            
+            
+    def predict(self, data):
+            weighted_input_hidden_layer = np.dot(self.weights_ih.transpose(), data) + self.bias_ih
+            first_layer = self.acf(weighted_input_hidden_layer)
         
-        weighted_input_output_layer = np.dot(np.transpose(self.weights_ho), first_layer) + self.bias_ho
-        output = self.acf(weighted_input_output_layer)
+            weighted_input_output_layer = np.dot(self.weights_ho.transpose(), first_layer) + self.bias_ho
+            prediction = self.acf(weighted_input_output_layer)
+            
+            return prediction
         
-        print ("Output is: ", output, "\n")
-        
-        delta_output = np.multiply(compute_errors_prime(output, self.targets), sigmoid_prime(weighted_input_output_layer))
-        delta_hidden = np.dot(np.dot(self.weights_ho, delta_output), sigmoid_prime(weighted_input_hidden_layer))
-        
-        self.weights_ho = self.weights_ho - np.multiply(self.lr, delta_output)
-        self.weights_ih = self.weights_ih - np.multiply(self.lr, delta_hidden)
-        
-#        print ("After update: ", self.weights_ih, "input -> hidden", "\n")
-#        print (self.weights_ho, "hidden -> output", "\n")
+
         
 def act_f(activation_function):
     if activation_function == 'tanh':
@@ -55,35 +68,42 @@ def act_f(activation_function):
     elif activation_function == 'sigmoid':
         return (sigmoid)
     else:
-        print("Sorry, the only activation functions currently supported are tanh and sigmoid")
-        print("\n")
+        raise NameError("Invalid activation function")
         
 def sigmoid(x):
-  return (1 / (1 + np.exp(-x)))
+    return (1 / (1 + np.exp(-x)))
+      
+def act_deriv(activation_function):
+    if activation_function == 'tanh':
+        return (1 - np.square(np.tanh))
+    elif activation_function == 'sigmoid':
+        return (sigmoid_prime)
+    else:
+        raise NameError("Invalid activation function")
 
 def sigmoid_prime(x):
     return (sigmoid(x)*(1 - sigmoid(x)))
     
-def compute_errors(pred, target):
-    return (float(1/2)*np.square(pred - target))
-    
 def compute_errors_prime(pred, target):
     return (pred - target)
     
-nr_of_nodes = 10
-act_fn = 'tanh'
-learning_rate = 0.3
-targets = np.random.rand(1)
+act_fn = 'sigmoid'
+learning_rate = 0.1
+targets = np.array([0, 1, 1, 0])
+batch_size = 1
 
-x = NeuralNet(nr_of_nodes, act_fn, learning_rate, targets)
+x = NeuralNet(act_fn, learning_rate, targets, batch_size)
 
-x.init_weights(10,3,1)
+x.init_weights(2,2,1)
 
-data = np.random.random(10)
-
+data = np.array([(1,1), (0,1), (1,0), (0,0)])
 i = 0
 
-while i < 100:
+while i < 100000:
     x.feed_forward(data)
     i += 1
     
+print(x.predict(np.array([0,0])))
+print(x.predict(np.array([0,1])))   
+print(x.predict(np.array([1,0])))
+print(x.predict(np.array([1,1]))) 